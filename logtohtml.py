6db3ce26f411f91614f36e941cb3d7194b99384c
posiputt <i@posiputt.de>
2015-03-17 00:13:13 +0100

    fixed ugly file handling

--- logtohtml.py
+++ logtohtml.py
@@ -4,9 +4,13 @@ if __name__ == '__main__':
     files = []
     lines_in = []
     lines_out = []
-    html_header = open('header', 'r').read()
-    lines_out.append(html_header)
-    html_footer = open('footer', 'r').read()
+    with open('header', 'r') as h:
+        html_header = h.read()
+        h.close()
+        lines_out.append(html_header)
+    with open('footer', 'r') as f:
+        html_footer = f.read()
+        f.close()
     for i, a in enumerate(sys.argv):
         if i != 0:
             files.append(a)
