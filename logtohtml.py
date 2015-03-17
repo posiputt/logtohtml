4e9ffa477f566bbd520a643e454257be3fde93e4
posiputt <i@posiputt.de>
2015-03-17 02:31:38 +0100

    handling of multiple files fixed. readability improvements.

--- logtohtml.py
+++ logtohtml.py
@@ -2,28 +2,28 @@ import sys
 
 if __name__ == '__main__':
     files = []
-    lines_in = []
-    lines_out = []
-    with open('header', 'r') as h:
-        html_header = h.read()
-        h.close()
-        lines_out.append(html_header)
-    with open('footer', 'r') as f:
-        html_footer = f.read()
-        f.close()
     for i, a in enumerate(sys.argv):
         if i != 0:
             files.append(a)
+    with open('header', 'r') as h:
+        html_header = h.read()
+        h.close()
+    with open('footer', 'r') as f:
+        html_footer = f.read()
+        f.close()
     for f in files:
-        filename_html = '.'.join([f.split('.')[0], 'html'])
+        lines_in = []
+        lines_out = []
+        lines_out.append(html_header)
+        filename_html = f.split('.')[0] + ".html"
         with open(f, 'r') as log:
             lines_in = log.read().split('\n')
             log.close()
         for l in lines_in:
-            l = '<p>' + l + '</p>\n'
+            l = '<p>' + l + '</p>'
             lines_out.append(l)
         lines_out.append(html_footer)
-        charbuf = ''.join(lines_out)
+        charbuf = '\n'.join(lines_out)
         with open(filename_html, 'w') as out:
             out.write(charbuf)
             out.close()
